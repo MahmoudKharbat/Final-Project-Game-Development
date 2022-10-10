4a8f4f15fee5316fc63df4929be241e034b17b47
Mahmoud Kharbat <mahmoodkharbat@gmail.com>
2022-10-11 00:27:47 +0300

    improving the swat animator

---
+++ Final Project/Assets/Scripts/npc's/npc_enemy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class npc_enemy : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public GameObject target;
+    public GameObject currNpc;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        agent.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (agent.enabled)
+            agent.SetDestination(target.transform.position);
+        if (animator.GetInteger("state") == 1)
+        {
+            agent.enabled = true;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (target.gameObject == other.gameObject)
+        {
+            Destroy(currNpc);
+        }
+    }
+}
